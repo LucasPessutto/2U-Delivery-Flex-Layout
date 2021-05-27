f18cb3928828171d25ab2a8e88248433e892ff09
Lucas Pessutto <lucas.pessutto1@gmail.com>
2021-05-26 22:08:40 -0300

    2UDelivery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component'
+import { EntregasComponent } from './pages/entregas/entregas.component'
+import { EstabelecimentoComponent } from './pages/estabelecimento/estabelecimento.component'
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },{
+    path: "entregas",
+    component: EntregasComponent
+  },{
+    path: "estabelecimento",
+    component: EstabelecimentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EntregasComponent } from './pages/entregas/entregas.component';
+import { EstabelecimentoComponent } from './pages/estabelecimento/estabelecimento.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    EntregasComponent,
+    EstabelecimentoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
